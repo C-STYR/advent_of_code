4bd61e421237417120837a11293b6b4aed66721e
Cole Styron <colestyron@gmail.com>
2022-12-01 10:23:56 -0900

    added 2022

---
+++ 2022/01/01.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	// "bufio"
+	"bufio"
+	"fmt"
+	"log"
+	"strconv"
+	// "io"
+	"os"
+)
+
+type Elf struct {
+	bag []int
+}
+
+func check(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
+func main() {
+	input, err := os.Open("./input/A_input.txt")
+	check(err)
+	defer input.Close()
+
+	elves := []int{}
+	bag := []int{}
+	
+	fileScanner := bufio.NewScanner(input)
+
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		// if line is not blank text 
+		if fileScanner.Text() != "" {
+			// convert to num and push into slice
+			num, _ := strconv.Atoi(fileScanner.Text())
+			bag = append(bag, num)
+			// fmt.Println(bag)
+		} else {
+			// if it is blank text
+			sum := 0
+			// sum slice and push into averages
+			for _, calorie := range bag {
+				sum += calorie
+			}
+			elves = append(elves, sum)
+			// empty slice
+			bag = nil
+		}
+	}
+
+	baddestElf := 0
+	for _, calories := range elves {
+		if calories > baddestElf {
+			baddestElf = calories
+		}
+	}
+	fmt.Println(baddestElf) //69883
+}
