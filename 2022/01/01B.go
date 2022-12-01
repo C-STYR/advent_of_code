ac80c28b791412b218466ff87c7abca5ceb21df0
Cole Styron <colestyron@gmail.com>
2022-12-01 11:17:17 -0900

    streamlined

--- 2022/01/01B.go
+++ 2022/01/01B.go
@@ -9,10 +9,6 @@ import (
 	"sort"
 )
 
-type Elf struct {
-	bag []int
-}
-
 func check(e error) {
 	if e != nil {
 		log.Fatal(e)
@@ -32,32 +28,21 @@ func main() {
 	fileScanner.Split(bufio.ScanLines)
 
 	for fileScanner.Scan() {
-		// if line is not blank text
 		if fileScanner.Text() != "" {
-			// convert to num and push into slice
 			num, _ := strconv.Atoi(fileScanner.Text())
 			bag = append(bag, num)
-			// fmt.Println(bag)
 		} else {
-			// if it is blank text
 			sum := 0
-			// sum slice and push into averages
 			for _, calorie := range bag {
 				sum += calorie
 			}
 			elves = append(elves, sum)
-			// empty slice
 			bag = nil
 		}
 	}
 
 	sort.Sort(sort.Reverse(sort.IntSlice(elves)))
-
 	baddestElf := elves[0]
-	
-	fmt.Println(baddestElf) //69883
-	
 	total := baddestElf + elves[1] + elves[2]
-	
-	fmt.Println(total) //207576
+	fmt.Println(baddestElf, total)
 }
