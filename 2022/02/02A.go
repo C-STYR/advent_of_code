660767b3180599b715c5e119b634e3329cad8144
Cole Styron <colestyron@gmail.com>
2022-12-02 10:49:48 -0900

    optimized

--- 2022/02/02A.go
+++ 2022/02/02A.go
@@ -3,66 +3,35 @@ package main
 import (
 	"bufio"
 	"fmt"
-
 	"log"
 	"os"
 )
 
-func logic(elf, me byte) int {
-	score := 0
-	if elf == 'A' {
-		if me == 'X' {
-			score += 4
-		}
-		if me == 'Y' {
-			score += 8
-		}
-		if me == 'Z' {
-			score += 3
-		}
-	}
-	if elf == 'B' {
-		if me == 'X' {
-			score += 1
-		}
-		if me == 'Y' {
-			score += 5
-		}
-		if me == 'Z' {
-			score += 9
-		}
-	}
-	if elf == 'C' {
-		if me == 'X' {
-			score += 7
-		}
-		if me == 'Y' {
-			score += 2
-		}
-		if me == 'Z' {
-			score += 6
-		}
-	}
-	return score
-}
-
 func main() {
+	scoreMap := map[string]int{
+		"A X": 4,
+		"A Y": 8,
+		"A Z": 3,
+		"B X": 1,
+		"B Y": 5,
+		"B Z": 9,
+		"C X": 7,
+		"C Y": 2,
+		"C Z": 6,
+	}
+
 	input, err := os.Open("./input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer input.Close()
 
-	fileScanner := bufio.NewScanner(input)
-
-	fileScanner.Split(bufio.ScanLines)
+	scanner := bufio.NewScanner(input)
 
 	output := 0
 
-	for fileScanner.Scan() {
-		elf := fileScanner.Text()[0]
-		me := fileScanner.Text()[2]
-		output += logic(elf, me)
+	for scanner.Scan() {
+		output += scoreMap[scanner.Text()]
 	}
 
 	fmt.Println(output)
--- 2022/02/02B.go
+++ 2022/02/02B.go
@@ -7,99 +7,24 @@ import (
 	"os"
 )
 
-func winLoseDraw(elf byte, WLD byte) byte {
-	// X = lose, Y = draw, Z = win
-	switch WLD {
-	case 'Z':
-		switch elf {
-		case 'A':
-			return 'Y'
-		case 'B':
-			return 'Z'
-		case 'C':
-			return 'X'
-		}
-	case 'X':
-		switch elf {
-		case 'A':
-			return 'Z'
-		case 'B':
-			return 'X'
-		case 'C':
-			return 'Y'
-		}
-	case 'Y':
-		switch elf {
-		case 'A':
-			return 'X'
-		case 'B':
-			return 'Y'
-		case 'C':
-			return 'Z'
-		}
-	}
-	return 'P'
-}
-
-func logic(elf, me byte) int {
-	score := 0
-	if elf == 'A' {
-		if me == 'X' {
-			score += 4
-		}
-		if me == 'Y' {
-			score += 8
-		}
-		if me == 'Z' {
-			score += 3
-		}
-	}
-	if elf == 'B' {
-		if me == 'X' {
-			score += 1
-		}
-		if me == 'Y' {
-			score += 5
-		}
-		if me == 'Z' {
-			score += 9
-		}
-	}
-	if elf == 'C' {
-		if me == 'X' {
-			score += 7
-		}
-		if me == 'Y' {
-			score += 2
-		}
-		if me == 'Z' {
-			score += 6
-		}
-	}
-	return score
-}
-
 func main() {
+	
+	scoreMap := map[string]int{"A X": 3, "A Y": 4, "A Z": 8, "B X": 1, "B Y": 5, "B Z": 9, "C X": 2, "C Y": 6, "C Z": 7}
+
+
 	input, err := os.Open("./input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer input.Close()
 
-	fileScanner := bufio.NewScanner(input)
-
-	fileScanner.Split(bufio.ScanLines)
+	scanner := bufio.NewScanner(input)
 
 	output := 0
 
-	for fileScanner.Scan() {
-		elf := fileScanner.Text()[0]
-		desiredOutcome := fileScanner.Text()[2]
-		myPlay := winLoseDraw(elf, desiredOutcome)
-
-		output += logic(elf, myPlay)
+	for scanner.Scan() {
+		output += scoreMap[scanner.Text()]
 	}
 
 	fmt.Println(output)
-
 }
